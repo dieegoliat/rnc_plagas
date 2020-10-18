0ec5b5794dcd3d3d5fe7bf9684b64baf2abedb51
dieegoliat <dieegoliat@gmail.com>
2020-10-17 23:50:10 -0300

    version2

---
+++ convolutional_neural_network_moscas_v2.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Convolutional Neural Network
+
+# ### Importing the libraries
+
+# In[1]:
+
+
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+
+
+# In[2]:
+
+
+tf.__version__
+
+
+# ## Part 1 - Data Preprocessing
+
+# ### Preprocessing the Training set
+
+# In[3]:
+
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+training_set = train_datagen.flow_from_directory(
+        'dataset_moscas/training_set',
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+
+# ### Preprocessing the Test set
+
+# In[4]:
+
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_set = test_datagen.flow_from_directory(
+        'dataset_moscas/test_set',
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+
+# ## Part 2 - Building the CNN
+
+# ### Initialising the CNN
+
+# In[5]:
+
+
+cnn = tf.keras.models.Sequential()
+
+
+# ### Step 1 - Convolution
+
+# In[6]:
+
+
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[150, 150, 3]))
+
+
+# ### Step 2 - Pooling
+
+# In[7]:
+
+
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+
+# ### Adding a second convolutional layer
+
+# In[8]:
+
+
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+
+# ### Step 3 - Flattening
+
+# In[9]:
+
+
+cnn.add(tf.keras.layers.Flatten())
+
+
+# ### Step 4 - Full Connection
+
+# In[10]:
+
+
+cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+
+
+# ### Step 5 - Output Layer
+
+# In[11]:
+
+
+cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+
+# ## Part 3 - Training the CNN
+
+# ### Compiling the CNN
+
+# In[12]:
+
+
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+# ### Training the CNN on the Training set and evaluating it on the Test set
+
+# In[13]:
+
+
+#cnn.fit(x = training_set, validation_data = test_set, epochs=25)
+cnn.fit(training_set, validation_data=test_set, epochs=10, steps_per_epoch=50, validation_steps=25)
+
+
+# Part 4 - Making a single prediction
+
+# In[44]:
+
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset_moscas/single_prediction/mosca_comun_or_frutos.1.jpg', target_size = (150, 150))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = cnn.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+  prediction = 'mosca frutos'
+else:
+  prediction = 'mosca común'
+
+
+# In[45]:
+
+
+print(prediction)
+
+
+# In[ ]:
+
+
+
+
